ee8e690c98515d85258fa7fa2e7c22735f173b2c
Bruno Martins <brunoferma@gmail.com>
2021-06-10 15:55:18 -0300

    Start Commint

--- app/src/main/java/com/example/superdados/MainActivity.kt
+++ app/src/main/java/com/example/superdados/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.superdados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,21 +12,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dados1 = findViewById(R.id.dados1) as TextView
-        val dados2 = findViewById(R.id.dados2) as TextView
+        val dados1 = findViewById(R.id.dado1) as ImageView
+        val dados2 = findViewById(R.id.dado2) as ImageView
         val btn = findViewById(R.id.btnJogar) as Button
 
+
+        val imagems = listOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+        )
         btn.setOnClickListener {
-            dados1.text = getNumber().toString()
-            dados2.text = getNumber().toString()
+            dados1.setImageResource(imagems.random())
+            dados2.setImageResource(imagems.random())
+
+
 
         }
-    }
 
-    private fun getNumber() : Int {
+            }
+
+    private fun getNumber(): Int {
         return (1..6).random()
 
+
     }
+
+
 }
+
 
 
