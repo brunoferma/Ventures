7ace90a026f0f81e201a97db60ab8d1863f324cd
Bruno Martins <brunoferma@gmail.com>
2021-06-14 11:17:16 -0300

    Commit2

--- app/src/main/java/com/example/superdados/MainActivity.kt
+++ app/src/main/java/com/example/superdados/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.superdados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val dados2 = findViewById(R.id.dado2) as ImageView
         val btn = findViewById(R.id.btnJogar) as Button
         val text = findViewById<TextView>(R.id.bemVindo)
+        val sharebutton = findViewById(R.id.share1) as FloatingActionButton
 
 
         val player = intent.getStringExtra("nome")
@@ -22,7 +27,19 @@ class MainActivity : AppCompatActivity() {
         val mensage = getString(R.string.bem_vindo, player)
         text.text = mensage
 
+        sharebutton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, "Voce e sortudo!!")
+            intent.setPackage("com.whatsapp")
+            intent.type = "text/plain"
 
+            if (intent.resolveActivity(this.packageManager) != null){
+                startActivity(intent)
+        } else {
+            Toast.makeText(this,"Voce nao possui Whast instalado", Toast.LENGTH_SHORT).show()
+        }
+
+    }
         val imagems = listOf(
             R.drawable.dice_1,
             R.drawable.dice_2,
